42cf4852b40fc9a1a0bf51a4826416654065673f
unknown <ST10250190@vcconnect.edu.za>
2024-11-22 13:15:46 +0200

    Adding the HR View to work as the front end of the HR Controller

--- obj/Debug/net8.0/PROG6212_POE.AssemblyInfo.cs
+++ obj/Debug/net8.0/PROG6212_POE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00606de5f09b118910522dde3635bac7ca3b50a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b3d2e5d98f93502002823c0985190b3a0db420")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
